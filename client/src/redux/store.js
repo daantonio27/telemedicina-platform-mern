c37eac0fac727f08a0a1c25828f8eacf5086f859
daantonio27 <daantonioc3@gmail.com>
2024-01-02 03:44:50 +0000

    Private Pubic Routes

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,8 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { alertSlice } from "./features/alertSlice";
 
 export default configureStore({
-    reducer:{
-        alerts: alertSlice.reducer
-    },
+  reducer: {
+    alerts: alertSlice.reducer,
+  },
 });
-
